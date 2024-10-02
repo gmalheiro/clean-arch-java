c0ac9e23b442d737c52a933984e0dc7bf204f316
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-10-02 01:35:37 -0300

    first commit add project to github

---
+++ src/main/java/br/com/gmalheiro/springboot_cleanarch/SpringbootCleanarchApplication.java
@@ -0,0 +1,13 @@
+package br.com.gmalheiro.springboot_cleanarch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCleanarchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCleanarchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gmalheiro/springboot_cleanarch/SpringbootCleanarchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gmalheiro.springboot_cleanarch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCleanarchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
